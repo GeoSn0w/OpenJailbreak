b893d0ab8ad2218e0d7aedd5e25fddb390af257a
GeoSn0w <GeoSn0w@users.noreply.github.com>
2020-08-10 07:24:38 -0700

    Legacy Xcode Support

---
+++ iOS Device Support/readme.m
@@ -0,0 +1,8 @@
+### Legacy Xcode Support Files
+
+This is a collection of Xcode iOS Support files which allow you to use an older Xcode version* to test your jailbreak tool even if you use an older macOS.
+For example, I use macOS High Sierra on an iMac 2011, running Xcode 10.1 as the latest supported version. This is OLD. Naturally it wouldn't support my iOS 13 devices, let alone my iOS 14 Beta ones. Adding the support files allows me to write code for much newer iOS than the default supported by my Xcode.
+
+This is rather useful if you're doing iOS development on a budget and you cannot afford a newer Mac or Macbook to have the latest Xcode.
+
+* In some cases iOS support files are not enough and you may also need a new SDK from a newer Xcode to support newer CPUs (i.e. A12 and A13).
